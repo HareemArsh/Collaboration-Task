20ae5b305673a62be9e56821a529a03422f0e8b9
HareemArsh <hareemkhawaja13@gmail.com>
2024-02-26 16:59:07 +0500

    tsk 1 added hareem

---
+++ task.py
@@ -0,0 +1,14 @@
+# Task 1  function that takes name of a user and returns Hello <Username>
+
+def greet_user(username):
+    return "Hello " + username
+
+# Example usage:
+user_name = input("Please enter your name: ")
+greeting_message = greet_user(user_name)
+print(greeting_message)
+
+# Task 2: a function that takes the date of birth of a user and returns age
+
+
+# Task 3: use the above functions to make the Command line app to simply take Name and Date of birth as input and print "Hello <Username>, your age is <calculated age> years"
